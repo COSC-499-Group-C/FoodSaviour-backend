b6c12a14050e92d9b831a6864d4e358155f84148
Anshul Dhariwal <anshuldhariwal04@gmail.com>
2023-02-03 11:26:48 -0800

    Added views and serializers

--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from .models import WasteType, TrackerData
+from .serializers import WasteTypeSerializer, TrackerDataSerializer
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.authentication import SessionAuthentication
+
+
+class WasteTypeViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Waste Type to be viewed or edited.
+    """
+    authentication_classes = [SessionAuthentication, ]
+    queryset = WasteType.objects.all()
+    serializer_class = WasteTypeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class TrackerDataViewSet(viewsets.ViewSet):
+    """
+    API endpoint that allows Tracker Data to be viewed or edited
+    """
+    authentication_classes = [SessionAuthentication, ]
+    queryset = TrackerData.objects.all()
+    serializer_class = TrackerDataSerializer
+    permission_classes = [permissions.IsAuthenticated]
