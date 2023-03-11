6cf233dee6ea777ce686a2db44f1497d517ea510
Anshul Dhariwal <anshuldhariwal04@gmail.com>
2023-03-11 10:37:14 -0800

    Updated filter for Tracker Data

--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.authentication import SessionAuthentication
+from django_filters import rest_framework as filters
 
 
 class WasteTypeViewSet(viewsets.ModelViewSet):
@@ -25,7 +26,9 @@ class TrackerDataViewSet(viewsets.ModelViewSet):
     queryset = TrackerData.objects.all()
     serializer_class = TrackerDataSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_fields = ('waste_type',)
 
     def get_queryset(self):
         user = self.request.user
-        return TrackerData.objects.filter(user_id=user.id).order_by('-created_at')
+        return self.queryset.filter(user_id=user.id).order_by('-created_at')
